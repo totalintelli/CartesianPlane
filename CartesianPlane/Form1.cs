70e06c5f33d5fc0fa37abf2cac53959b9b12607e
totalintelli <blueintelli@gmail.com>
2019-01-11 12:31:19 +0900

    Make Graph Class.

--- CartesianPlane/Form1.cs
+++ CartesianPlane/Form1.cs
@@ -25,7 +25,6 @@ namespace CartesianPlane
         private void DrawEdge()
         {
             Graphics graphics = this.graph_panel.CreateGraphics();
-
             Pen pen = new Pen(Color.Black, thick);
             int startPointX = 0; // 테두리를 그리는 시작점의 x 좌표
             int startPointY = 0; // 테두리를 그리는 시작점의 y 좌표
@@ -34,19 +33,48 @@ namespace CartesianPlane
             graphics.DrawRectangle(pen, startPointX, startPointY, width - (thick + startPointX), height - (thick + startPointY));
             graphics.Dispose();
         }
+        
+
+        /// <summary>
+        /// 그래프를 패널에 그린다.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void graph_panel_Paint(object sender, PaintEventArgs e)
+        {
+            // 그래프 경계선을 그린다.
+            DrawEdge();
+
+            XYGraph graph = new XYGraph();
+            // 그래프의 X축과 Y축을 그린다.
+            graph.DrawAxis(this.graph_panel, 10, thick);
+            // y = x 그래프를 그린다.
+            graph.DrawGraph();
+        }
+
+        
+    }
+
+    public class XYGraph
+    {
+        int x = 0; // X 축의 x 좌표
+        int y = 0; // X 축의 y 좌표
 
         /// <summary>
         /// X축과 Y축을 그린다.
         /// </summary>
-        private void DrawAxis(int padding)
+        /// <param name="panel">그래프를 그릴 패널</param>
+        /// <param name="padding">그래프 안쪽 간격</param>
+        /// <param name="thick">그래프 두께</param>
+        public void DrawAxis(Panel panel, int padding, int thick)
         {
-            Graphics graphics = this.graph_panel.CreateGraphics();
+
+            Graphics graphics = panel.CreateGraphics();
             Pen pen = new Pen(Color.Black);
-            int x = 0; // X 축의 x 좌표
-            int y = 0; // X 축의 y 좌표
+           
             int outerPadding = padding + thick;
-            int width = this.graph_panel.Width;
-            int height = this.graph_panel.Height;
+            int width = panel.Width; // 패널의 너비
+            int height = panel.Height; // 패널의 너비
 
             // X축을 그린다.
             graphics.DrawLine(pen, x + outerPadding, height - outerPadding, width - outerPadding, height - outerPadding);
@@ -57,13 +85,39 @@ namespace CartesianPlane
             graphics.Dispose();
         }
 
-        private void graph_panel_Paint(object sender, PaintEventArgs e)
+        /// <summary>
+        /// y = x 그래프를 그린다.
+        /// </summary>
+        public void DrawGraph()
         {
-            // 그래프 경계선을 그린다.
-            DrawEdge();
+            // 그래프의 시작점
+            Point graphStartingPoint = new Point();
+            // 그래프의 끝점
+            Point graphEndPoint = new Point();
+            // 패널에 표시한 그래프의 시작점(원점)을 구한다.
+            Point startingPoint = GetStartingPoint(graphStartingPoint);
+            // 패널에 표시한 그래프의 끝점을 구한다.
+            Point endPoint = GetEndPoint(graphEndPoint);
+        }
 
-            // 그래프의 X축과 Y축을 그린다.
-            DrawAxis(10);
+        /// <summary>
+        /// 패널에 표시한 그래프의 끝점을 구한다.
+        /// </summary>
+        /// <param name="graphEndPoint">그래프의 끝점</param>
+        private Point GetEndPoint(Point graphEndPoint)
+        {
+            Point endPoint = new Point();
+            return endPoint;
+        }
+
+        /// <summary>
+        /// 패널에 표시한 그래프의 시작점을 구한다.
+        /// </summary>
+        /// <param name="startingPoint">그래프의 시작점</param>
+        private Point GetStartingPoint(Point graphStartingPoint)
+        {
+            Point startingPoint = new Point();
+            return startingPoint;
         }
     }
 }
